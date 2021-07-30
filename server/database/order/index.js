046c2c0a49dda128f7ddea73ca0601c993f1eb42
prakhar0211 <prakhar2nov@gmail.com>
2021-07-30 14:34:58 +0530

    add model and schema

---
+++ server/database/order/index.js
@@ -0,0 +1,27 @@
+import mongoose from "mongoose"
+
+const OrderSchema = new mongoose.Schema({
+    user: {
+        type: mongoose.Types.ObjectId,
+        ref: "Users"
+    },
+    orderDetails: [
+        {
+            food: {
+                type: mongoose.Types.ObjectId,
+                ref: "Foods"
+            },
+            quantity: { type: Number, required: true },
+            paymode: { type: String, required: true },
+            sttus: { type: String, default: "Placed" },
+            paymentDetails: {
+                itemTotal: { type: Number, required: true },
+                promo: { type: Number, required: true }, //amount of discount offered
+                tax: { type: Number, required: true },
+            }
+        }
+    ],
+    orderRatings:{type:Number,required:true},
+})
+
+export const OrderModel = mongoose.Model("Orders", OrderSchema);
