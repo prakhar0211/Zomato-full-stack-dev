adc12f91a0d183648c3c5eaeb410f9ab7bdbf7ee
prakhar0211 <prakhar2nov@gmail.com>
2023-04-17 15:53:08 +0530

    add signin route

---
+++ server/API/Auth/index.js
@@ -0,0 +1,46 @@
+// Library
+import express from "express";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+
+// Models
+import { UserModel } from "../../database/user";
+
+const Router = express.Router();
+
+/*
+Route  /signup
+Des     Signup with email and password
+params  none
+Access  public
+Method  POST
+*/
+Router.post("/signup", async (req, res) => {
+    try {
+        await UserModel.findByEmailAndPhone(req.body.credentials);
+        const newUser = await UserModel.create(req.body.credentials);
+        const token = newUser.generateJwtToken();
+        return res.status(200).json({ token, status: "success" });
+    } catch (error) {
+        return res.status(500).json({ error: error.message });
+    }
+});
+
+/*
+Route  /signin
+Des     Signin with email and password
+params  none
+Access  public
+Method  POST
+*/
+Router.post("/signin", async (req, res) => {
+    try {
+        const user = await UserModel.findByEmailAndPassword(req.body.credentials);
+        const token = user.generateJwtToken();
+        return res.status(200).json({ token, status: "success" });
+    } catch (error) {
+        return res.status(500).json({ error: error.message });
+    }
+});
+
+export default Router;
---
+++ server/database/reviews/index.js
@@ -0,0 +1,30 @@
+import mongoose from "mongoose";
+
+const ReviewSchema = new mongoose.Schema(
+    {
+        user: {
+            type: mongoose.Types.ObjectId,
+            ref: "Users",
+        },
+        food: {
+            type: mongoose.Types.ObjectId,
+            ref: "Foods",
+        },
+        restaurant: {
+            type: mongoose.Types.ObjectId,
+            ref: "Restaurants",
+        },
+        rating: { type: Number, required: true },
+        reviewText: { type: String, required: true },
+        photos: [
+            {
+                type: mongoose.Types.ObjectId,
+                ref: "Images",
+            },
+        ],
+    },
+    {
+        timestamps: true,
+    });
+
+export const ReviewModel = mongoose.model("Reviews", ReviewSchema);
